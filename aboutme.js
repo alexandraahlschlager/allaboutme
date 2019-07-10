3cc33bd857bf9936d2422efdf07436d4ae433e51
alexandraahlschlager <52722852+alexandraahlschlager@users.noreply.github.com>
2019-07-10 09:34:25 -0700

    Add files via upload

---
+++ aboutme.js
@@ -0,0 +1,10 @@
+function changeImage()
+{
+document.images["jsbutton"].src= "https://i.pinimg.com/originals/82/4f/84/824f84d100e5189a6b9d6607362772f1.jpg";
+return true;
+}
+function changeImageBack()
+{
+ document.images["jsbutton"].src = "https://66.media.tumblr.com/fb99de4cdfeafa2066a08149093a2435/tumblr_oxirm34F471w8f1tco1_250.jpg";
+ return true;
+}
